4795598a8c045daf6265a3fe87a302047116613c
Yasumii007 <yasmimalvesrodrigues16@gmail.com>
2025-04-04 22:33:23 -0300

    ajuste de variaveis, para melhor entendimento e organizacao

--- Program.cs
+++ Program.cs
@@ -1,14 +1,14 @@
-﻿double adutBebem, adultNãBebem, crianca, carne, acomp, cerv, refri, agua;
+﻿double adultB, adultNB, crianca, carne, acomp, cerv, refri, agua;
 
 
 Console.WriteLine("Bem-vindo ao Churrasco Feliz \n");
 Console.WriteLine("Por favor preencha os dados abaixo:\n");
 
 Console.Write("Por favor digite quantos adultos consumidores de alcool vão: ");
-adutBebem = Convert.ToInt32(Console.ReadLine()!);
+adultB = Convert.ToInt32(Console.ReadLine()!);
 
 Console.Write("\nPor favor digite quantos adultos não consumidores de alcool vão: ");
-adultNãBebem = Convert.ToInt32(Console.ReadLine()!);
+adultNB = Convert.ToInt32(Console.ReadLine()!);
 
 Console.Write("\nPor favor digite quantas crianças vão: ");
 crianca = Convert.ToInt32(Console.ReadLine()!);
@@ -17,19 +17,19 @@ Console.Clear();
 
 Console.WriteLine("--- Churrasco ---");
 
-Console.WriteLine($"Adultos (que consomem bebidas alcoólicas).......: {adutBebem}");
-Console.WriteLine($"Adultos (que não consomem bebidas alcoólicas)...: {adultNãBebem}");
+Console.WriteLine($"Adultos (que consomem bebidas alcoólicas).......: {adultB}");
+Console.WriteLine($"Adultos (que não consomem bebidas alcoólicas)...: {adultNB}");
 Console.WriteLine($"Crianças........................................: {crianca}  \n");
 
-carne = ((adultNãBebem + adutBebem) * 0.4) + (crianca * 0.2); 
+carne = ((adultNB + adultB) * 0.4) + (crianca * 0.2); 
 
-acomp = (adultNãBebem + adutBebem + crianca) * 0.2;
+acomp = (adultNB + adultB + crianca) * 0.2;
 
-cerv = adutBebem * 2;
+cerv = adultB * 2;
 
-refri = (adultNãBebem + crianca) * 0.5;
+refri = (adultNB + crianca) * 0.5;
 
-agua = (adultNãBebem + adutBebem + crianca) * 0.4;
+agua = (adultNB + adultB + crianca) * 0.4;
 
 Console.WriteLine($"Carne.............: {carne:F1}Kg");
 Console.WriteLine($"Acompanhamentos...: {acomp:F1}Kg");
